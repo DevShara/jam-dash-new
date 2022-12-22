09b234bf30cf6a97774f14d1a1db6f27ca479cb0
DevShara <developer.ishara@gmail.com>
2022-12-22 17:51:06 +0530

    profile page  encapsulation

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { useIdentityContext } from 'react-netlify-identity';
 
 const Profile = () => {
-    return(
+
+  const identity = useIdentityContext();
+  const isLoggedIn = identity && identity.isLoggedIn;
+  console.log(identity);
+
+    return (
+      isLoggedIn &&
+      (
         <div className="dashboard-header">
-        <nav>
-          <Link to="/dashboard/secret" activeClassName="active">
-            Secret Stuff
-          </Link>
-          <Link to="/dashboard/base" activeClassName="active">
-            See Your Base
-          </Link>
-        </nav>
-       
-      </div>
+          <nav>
+            <Link to="/dashboard/secret" activeClassName="active">
+              Secret Stuff
+            </Link>
+            <Link to="/dashboard/base" activeClassName="active">
+              See Your Base
+            </Link>
+          </nav>
+        </div>
+      )
     )
 };
 
